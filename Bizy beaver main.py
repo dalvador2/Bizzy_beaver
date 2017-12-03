c084b70ad4c43cc688e2f23c4732be73abeba1f3
Dalvador2 <Torin@storkey.org>
2017-12-03 16:33:30 +0000

    Tested
    I have tested it with 1 card and it works.

--- Bizy beaver main.py
+++ Bizy beaver main.py
@@ -1,7 +1,25 @@
 def txt_array(file):
+    lines=0
+    for line in file:
+        lines+=1
     array=[]
     array.append(1)
-    sub_array=[]
+    for i in range(round(lines/4)):
+        array.append([])
+    file.seek(0)
     counter=0
     for line in file:
+        if counter==0:
+            card_no=int(line[5])
+        elif counter in [1,2]:
+            subarray=line.split(" ")
+            subarray=list(map(int,subarray))
+            array[card_no].append(subarray)
+        else:
+            pass
+        counter=(counter+1)%4
+    return array
         
+file=open("input.txt","r")
+array=txt_array(file)
+print(array)
