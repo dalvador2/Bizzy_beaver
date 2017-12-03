597f8648507b34e78fddb92b81f0d36b63fd8bf1
Dalvador2 <Torin@storkey.org>
2017-12-03 17:18:29 +0000

    Runing works
    Tested with 1 card and it works without crashing

--- Bizy beaver main.py
+++ Bizy beaver main.py
@@ -19,12 +19,25 @@ def txt_array(file):
             pass
         counter=(counter+1)%4
     return array
-     
+
+def run_beaver(tape,instruction,beaver_position,current_card,reps):
+    for i in range(reps):
+        if current_card==0:
+            break
+        value=tape[beaver_position]
+        tape[beaver_position]=instruction[current_card][value][0]
+        if instruction[current_card][value][0]==0:
+            beaver_position=beaver_position-1
+        else:
+            beaver_position+=1
+        current_card_no=instruction[current_card][value][2]
+    return tape,instruction,beaver_position,current_card
 file=open("input.txt","r")
 instruction_array=txt_array(file)
 tape=[]
-for i in range(10000):
+card_no=1
+for i in range(1000):
     tape.append(0)
-beaver_position=5000
+beaver_position=500
+tape,instruction_array,beaver_position,card_no=run_beaver(tape,instruction_array,beaver_position,card_no,10)
 
-print(array)
